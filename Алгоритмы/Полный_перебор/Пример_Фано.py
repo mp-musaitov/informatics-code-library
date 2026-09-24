a6144df7db5c5610609a12dd6410ad4900f50347
Magomed Musaitov <musaitovgpt@gmail.com>
2026-09-24 20:14:51 +0300

    Унифицировать проверку условия Фано

--- Алгоритмы/Полный_перебор/Пример_Фано.py
+++ Алгоритмы/Полный_перебор/Пример_Фано.py
@@ -1,7 +1,7 @@
 def fano(codes):
-    for a in codes:
-        for b in codes:
-            if a != b and b.startswith(a):
+    for i in range(len(codes)):
+        for j in range(len(codes)):
+            if i != j and codes[j].startswith(codes[i]):
                 return False
     return True
 
